126a9fe6af619b0bf4cce4af79de1cf03483e045
MoltenPL85 <bugaev.maxim.85@gmail.com>
2018-06-20 13:44:43 +0500

    Note-App push to github

---
+++ src/edit.js
@@ -0,0 +1,35 @@
+import { initializeEditPage, generateLastEdited } from './views';
+import { updateNote, removeNote } from './notes';
+
+const titleEl = document.querySelector('#note-title');
+const dateEl = document.querySelector('#last-edited');
+const bodyEl = document.querySelector('#note-body');
+const removeEl = document.querySelector('#remove-note');
+const noteId = location.hash.substring(1);
+
+initializeEditPage(noteId);
+
+titleEl.addEventListener('input', e => {
+  const note = updateNote(noteId, {
+    title: e.target.value,
+  });
+  dateEl.textContent = generateLastEdited(note.updatedAt);
+});
+
+bodyEl.addEventListener('input', e => {
+  const note = updateNote(noteId, {
+    body: e.target.value,
+  });
+  dateEl.textContent = generateLastEdited(note.updatedAt);
+});
+
+removeEl.addEventListener('click', () => {
+  removeNote(noteId);
+  location.assign('/index.html');
+});
+
+window.addEventListener('storage', e => {
+  if (e.key === 'notes') {
+    initializeEditPage(noteId);
+  }
+});
